b6bc8f8e8b30f7c56b231dcc403b1e527e02b49c
Ilyas Dzhalilov <i.dzhalilov@redmadrobot.com>
2023-11-19 20:13:33 +0300

    init blocking

--- src/main/java/com/example/testreactive/controller/BlockingController.java
+++ src/main/java/com/example/testreactive/controller/BlockingController.java
@@ -13,22 +13,22 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping(value = "/block", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BlockingController {
-//    private final RestTemplate restTemplate;
-//
-//    @Autowired
-//    public BlockingController(RestTemplateBuilder builder) {
-//        this.restTemplate = builder.build();
-//    }
-//
-//    @GetMapping("/fast")
-//    public Info getFromFastClient() {
-//        return restTemplate.exchange("http://localhost:8081/info", HttpMethod.GET, null, Info.class)
-//                .getBody();
-//    }
-//
-//    @GetMapping("/slow")
-//    public Info getFromSlowClient() {
-//        return restTemplate.exchange("http://localhost:8082/info", HttpMethod.GET, null, Info.class)
-//                .getBody();
-//    }
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public BlockingController(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
+    @GetMapping("/fast")
+    public Info getFromFastClient() {
+        return restTemplate.exchange("http://localhost:8081/info", HttpMethod.GET, null, Info.class)
+                .getBody();
+    }
+
+    @GetMapping("/slow")
+    public Info getFromSlowClient() {
+        return restTemplate.exchange("http://localhost:8082/info", HttpMethod.GET, null, Info.class)
+                .getBody();
+    }
 }
