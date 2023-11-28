3f10a4d99e1100f1aa75bf6638f90068134e25cd
Ilyas Dzhalilov <i.dzhalilov@redmadrobot.com>
2023-11-28 09:42:35 +0300

    fix warnings

--- src/main/java/com/example/testreactive/TestReactiveApplication.kt
+++ src/main/java/com/example/testreactive/TestReactiveApplication.kt
@@ -1,11 +1,11 @@
 package com.example.testreactive
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TestReactiveApplication {
-    fun main(args: Array<String>) {
-        SpringApplication.run(TestReactiveApplication::class.java, *args)
-    }
+class TestReactiveApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestReactiveApplication>(*args)
 }
